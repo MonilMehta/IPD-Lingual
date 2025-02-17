e46d68890f27959f71ffb50626c72a7dfd903a68
Monil Mehta <monilmehta5@gmail.com>
2025-02-18 00:19:36 +0530

    Delete lingual directory

--- lingual/app/(main)/camera.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { ModelView } from '../../src/ModelView';
-
-export default function CameraRoute() {
-  console.log('Control in main/camera')
-  return <ModelView />;
-}
--- lingual/react-native-fs.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  readDir: async () => [],
-  readFile: async () => '',
-  exists: async () => false,
-  // Add additional stub methods if needed
-};
